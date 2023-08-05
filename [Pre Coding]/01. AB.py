1c1671c320a8423251c98c09ec8cf6a4a90377c3
Peerawich Pichayathadapong <pprwfwithsleeping.pills@gmail.com>
2023-08-05 23:01:46 +0700

    Lab | Pre-Test

--- [Pre Coding]/01. AB.py
+++
@@ -1,6 +0,0 @@
-'''Sumation'''
-
-def fusion(num1):
-    '''sum'''
-    print(num1 + int(input()))
-fusion(int(input()))
--- [Pre Coding]/02. Hamburger.py
+++
@@ -1,6 +0,0 @@
-'''ham'''
-
-def burger(left, right):
-    '''burger'''
-    print("|" * left + "**" * (left + right) + "|" * right)
-burger(int(input()), int(input()))
