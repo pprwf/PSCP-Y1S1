a3a15985b5865dfbc2a5c5e1f8c22add61069c4a
Peerawich Pichayathadapong <pprwfwithsleeping.pills@gmail.com>
2023-09-07 21:15:40 +0700

    Lab | Week 02 (Composite Function & Condition)

--- PSCP2023 (IT21)/High Code/Lab 02/16. WeightStation.py
+++ PSCP2023 (IT21)/High Code/Lab 02/16. WeightStation.py
@@ -4,6 +4,6 @@ def station(avg, tire, tire2, tire3):
     '''WeightStation'''
     high, low, miss = round(avg + avg / 2), round(avg - avg / 2) + 1, avg * 4 - tire - tire2 - tire3
     print("Overweight" if avg * 4 > 15000 else "Unbalance"\
-    if tire not in range(low, high) or tire2 not in range(low, high) or tire3 not in range(low, high) or\
-    miss not in range(low, high) else "Pass %.2f" %miss)
+    if tire not in range(low, high) or tire2 not in range(low, high) or \
+    tire3 not in range(low, high) or miss not in range(low, high) else "Pass %.2f" %miss)
 station(float(input()), float(input()), float(input()), float(input()))
