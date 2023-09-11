f579ad52726213ebc3515b260ef6c116628d2c71
Peerawich Pichayathadapong <pprwfwithsleeping.pills@gmail.com>
2023-09-11 23:50:47 +0700

    Lab | Week 02 (Composite Function & Condition) Not clear

--- PSCP2023 (IT21)/Normal Code/Lab 02/18. SurprisingVote.py
+++ PSCP2023 (IT21)/Normal Code/Lab 02/18. SurprisingVote.py
@@ -4,7 +4,7 @@ def vote():
     '''SurprisingVote'''
     total = float(input())
     high = float(input())
-    least = (total - high) / 2
+    least = (total - high) // 2
     if high * 2 < total:
         least = total - high * 2
     if high - least > 2:
